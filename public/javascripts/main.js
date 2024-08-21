3c5d6d5ee5ece68dd87b64f6f33e9772f7e24966
Pranav Bhavsar <122773238+pranavbhavsar082003@users.noreply.github.com>
2024-08-21 02:12:04 -0400

    done

---
+++ public/javascripts/main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const toggler = document.querySelector('.navbar-toggler');
+    const menu = document.querySelector('.navbar-menu');
+  
+    toggler.addEventListener('click', function () {
+      menu.classList.toggle('active');
+    });
+  });
